44261433f63598d4d8420a4848955e9356363b17
AishwaryaHiremath22 <aishwaryash97@gmail.com>
2018-05-09 22:53:26 +0530

    changed user model and traversal.py and added relations1.py

--- RecApp/models.py
+++ RecApp/models.py
@@ -1,64 +1,63 @@
 from django.db import models
-from neomodel import (BooleanProperty,config,JSONProperty,FloatProperty,StructuredNode, StructuredRel,StringProperty, IntegerProperty,UniqueIdProperty, RelationshipTo, RelationshipFrom,EmailProperty,Relationship)
+from neomodel import (BooleanProperty, config, JSONProperty, FloatProperty, StructuredNode, StructuredRel, \
+                      StringProperty, IntegerProperty, UniqueIdProperty, RelationshipTo, RelationshipFrom,
+                      EmailProperty, Relationship)
 # Create your models here.
 # config.DATABASE_URL = 'bolt://neo4j:Recommender@localhost:11002'
 
-
 class Price(StructuredNode):
-    cost = FloatProperty(unique = True)
+    cost = FloatProperty(unique=True,required=False)
+
 
 class Cuisine(StructuredNode):
     cid = UniqueIdProperty()
     name = StringProperty()
 
+
 class Location(StructuredNode):
     locality = StringProperty()
+
 
 class Locrel(StructuredRel):
     addr = StringProperty(required=True)
     lat = FloatProperty()
     long = FloatProperty()
-    #zipcode = IntegerProperty(NULL = True)
+    # zipcode = IntegerProperty(NULL = True)
+
 
 class Restaurant(StructuredNode):
     rid = UniqueIdProperty()
     name = StringProperty()
     avg_cost = FloatProperty()
     rating = FloatProperty()
-    ratings_count  = IntegerProperty()
+    ratings_count = IntegerProperty()
     url = StringProperty()
     has_online_delivery = BooleanProperty()
     has_table_booking = BooleanProperty()
 
-    SERVES = RelationshipTo(Cuisine,'SERVES')
-    IN = RelationshipTo(Location,'IN',model=Locrel)
-    WITH = RelationshipTo(Price,'WITH_COST')
-    def find_res_with_cuisine(self):
-        query = """MATCH (n:Restaurant)->[:SERVES]->(c:Cuisine)<-[:SERVES]<-(m:Restaurant)
-                   WHERE n<>m 
-                   COLLECT(c.name) AS CUISINES return CUISINES,m"""
-        res = self.cypher(query)
-        print(res)
-    #IN = RelationshipTo(Location,'IN')
+    SERVES = RelationshipTo(Cuisine, 'SERVES')
+    IN = RelationshipTo(Location, 'IN', model=Locrel)
+    WITH = RelationshipTo(Price, 'WITH_COST')
 
 class Rated(StructuredRel):
     rating = FloatProperty()
 
+
 class Liked(StructuredRel):
     liking = IntegerProperty()
 
+
 class Sim(StructuredRel):
     similarity = FloatProperty()
-    
-class User(StructuredNode):
-    uid = UniqueIdProperty()
-    name = StringProperty()
-    phone_no = StringProperty()
-    email_id = EmailProperty()
-    age = IntegerProperty()
-    gender = StringProperty()
+    count = IntegerProperty()
 
-    FRIEND = Relationship('User','FRIENDS')
-    RATED = RelationshipTo(Restaurant,'RATED',model = Rated)
-    LIKES = RelationshipTo(Cuisine,'LIKES',model = Liked)
-    SIM = RelationshipTo(Restaurant,'SIMILARITY',model = Sim)
+class User(StructuredNode):
+    name = StringProperty()
+    FRIEND = Relationship('User', 'FRIENDS')
+    RATED = RelationshipTo(Restaurant, 'RATED', model=Rated)
+    LIKES = RelationshipTo(Cuisine, 'LIKES', model=Liked)
+    SIM = RelationshipTo(Restaurant, 'SIMILARITY', model=Sim)
+
+
+
+
--- RecApp/traversals.py
+++ RecApp/traversals.py
@@ -1,80 +1,64 @@
-import csv, random, os
-import django
-import sys
-import pandas as pd
-import py2neo
-import json
-import requests
-from models import *
-from neomodel import (config, StructuredNode, StringProperty, IntegerProperty, UniqueIdProperty, RelationshipTo,
-                      RelationshipFrom,db)
-from py2neo import Graph, Node, Relationship
+from neomodel import db, config
 
-# graph = Graph('http://localhost:11001/db/data')
-config.DATABASE_URL = 'bolt://neo4j:Test@localhost:11002'
+config.DATABASE_URL = 'bolt://neo4j:aishwarya@localhost:7687'
 
-def find_res_with_cuisine():
-		"""Find restaurants that serve same cuisine"""
-		query = """
-					MATCH (n:Restaurant)-[:SERVES]->(c:Cuisine)<-[:SERVES]-(m:Restaurant)
-					WHERE n<>m 
-					WITH n,COLLECT(c.name) AS CUISINES,m return n.name,CUISINES,m.name
-				"""
-		r,res = db.cypher_query(query)
-		for i in range(0,len(r)):
-			print(r[i])
+def friend_similarity(uname, rate):
+    params = {
+        'name': uname,
+        'rate_res': float(rate),
+    }
 
-def friend_similarity(uname,rate):
-	params = {
-	'name' : uname,
-	'rate_res' : float(rate),
-	}
-	
-	"""Find Restaurants that my friends have rated 3 and above and that serve the cuisines that I like"""
-	query = """	MATCH (u:User{name:$name})-[FRIENDS]->(rec:User),
+    """Find Restaurants that my friends have rated 3 and above and that serve the cuisines that I like"""
+    query = """	MATCH (u:User{name:$name})-[FRIENDS]->(rec:User),
 				(rec)-[r:RATED]-(res:Restaurant),(u)-[l:LIKES]->(c:Cuisine)<-[:SERVES]-(res) 
 				where r.rating > $rate_res with res,r return res.name"""
-	res,meta = db.cypher_query(query,params)
-	for i in range(0,len(res)):
-		print(res[i])
-	
-def find_res_with_cusine_para(cuisine):
-    for j in cuisine:
-        query = "MATCH (n:Restaurant)-[:SERVES]->(c:Cuisine) WHERE c.name='" + j + "' RETURN n.name,c.name"
-        r, res = db.cypher_query(query)
-        for i in range(0, len(r)):
-            print(r[i])
+    res, meta = db.cypher_query(query, params)
+    for i in range(0, len(res)):
+        print(res[i])
 
-def jaccard_similarity(rname):
-	"""Find similar restaurants in terms of price range, cuisine and locality"""
-	query = """MATCH (r:Restaurant)-[:SERVES|:IN|:WITH_COST]->(n)<-[:SERVES|:IN|:WITH_COST]-(other:Restaurant)
-				with r,other,count(n) as intersection 
-				MATCH (r)-[:SERVES|:IN|:WITH_COST]->(r1)
-				with r,other,intersection,collect(distinct(id(r1))) as u1
-				MATCH (other)-[:SERVES|:IN|:WITH_COST]->(r2)
-				with r,other,intersection,u1,collect(distinct(id(r2))) as u2
-				with r,other,intersection,u1,u2
-				with r,other,intersection,u1 + filter(x in u2 WHERE NOT  x IN u1) as union ,u1,u2
-				return r.name,other.name,((1.0*intersection)/(size(union))) as jaccard order by jaccard desc limit 20"""
-	res,meta = db.cypher_query(query)
-	for i in range(0,len(res)):
-		print(res[i])
-			
-jaccard_similarity("Farzi Cafe")
 
-def colaborative():
-    query = "MATCH (p1:User)-[x:RATED]->(r:Restaurant)<-[y:RATED]-(p2:User) WITH SUM(x.rating * y.rating) AS xyDotProduct,\
+def jaccard_similarity(rlist):
+    for rname in rlist:
+        params = {
+            'rname':rname,
+        }
+        """Find similar restaurants in terms of price range, cuisine and locality"""
+        query = "MATCH (r:Restaurant {name:'"+rname+"'})-[:SERVES|:IN|:WITH_COST]->(n)<-[:SERVES|:IN|:WITH_COST]-(other:Restaurant)\
+                    with r,other,count(n) as intersection \
+                    MATCH (r)-[:SERVES|:IN|:WITH_COST]->(r1)\
+                    with r,other,intersection,collect(distinct(id(r1))) as u1\
+                    MATCH (other)-[:SERVES|:IN|:WITH_COST]->(r2) \
+                    with r,other,intersection,u1,collect(distinct(id(r2))) as u2 \
+                    with r,other,intersection,u1,u2 \
+                    with r,other,intersection,u1 + filter(x in u2 WHERE NOT  x IN u1) as union ,u1,u2 \
+                    return r.name,other.name,((1.0*intersection)/(size(union))) as jaccard order by jaccard desc limit 3"
+        res, meta = db.cypher_query(query)
+        for i in range(0, len(res)):
+            print(res[i])
+
+
+def colaborative(uname):
+    params ={
+        'uname':uname,
+    }
+    query = "MATCH (p1:User {name:'"+uname+"'})-[x:RATED]->(rest:Restaurant)<-[y:RATED]-(p2:User) WITH SUM(x.rating * y.rating) AS xyDotProduct,\
+    count(rest) as c,\
     SQRT(REDUCE(xDot = 0.0, a IN COLLECT(x.rating) | xDot + a^2)) AS xLength,\
     SQRT(REDUCE(yDot = 0.0, b IN COLLECT(y.rating) | yDot + b^2)) AS yLength,p1, p2 MERGE (p1)-[s:SIMILARITY]-(p2)\
-    SET s.similarity = xyDotProduct / (xLength * yLength)"
+    SET s.similarity = (xyDotProduct) / (xLength * yLength),s.count = c"
     r,res = db.cypher_query(query)
-    query1 = "MATCH (p1:User {name:'Aishwarya'})-[s:SIMILARITY]-(p2:User) WITH p2, s.similarity AS sim ORDER BY sim DESC LIMIT 5 \
+    query1 = "MATCH (p1:User {name:'"+uname+"'})-[s:SIMILARITY]-(p2:User) WITH p2, s.similarity AS sim,s.count as c ORDER BY sim*c DESC LIMIT 5 \
     RETURN p2.name AS Neighbor, sim AS Similarity"
     r1,res = db.cypher_query(query1)
-    for i in range(0,len(r1)):
-        print(r1[i])
-
-colaborative()
+    for i in r1:
+        print(i)
+        query2 = "MATCH (o:User {name: '"+i[0]+"'})-[r:RATED]->(rec:Restaurant)\
+                    MATCH (u:User{name: '"+uname+"'}) WHERE NOT EXISTS( (u)-[:RATED]->(rec) ) AND r.rating > 4 RETURN rec.name"
+        r3,res = db.cypher_query(query2)
+        for j in range(0,len(r3)):
+            print(r3[j])
+    query3 = "MATCH (p1:User {name:'"+uname+"'})-[s:SIMILARITY]-() DELETE s"
+    r3,res=db.cypher_query(query3)
 
 def weighted_content(cuisine,loc):
     params = {
@@ -89,6 +73,7 @@ def weighted_content(cuisine,loc):
     for i in range(0, len(r)):
         print(r[i])
 
-weighted_content('American','Church Street')
-#find_res_with_cusine_para(['Italian','American'])	
+#weighted_content('American','Church Street')
+#colaborative('Saloni')
+#jaccard_similarity(['Koramangala Social','Paakashala'])
 #friend_similarity(sys.argv[1],sys.argv[2])
